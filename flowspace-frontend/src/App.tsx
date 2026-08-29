317a98667da2a99bf5bc27efa82089ea3a290443
Amit-23 <amitsanghalsingh@gmail.com>
2026-08-30 00:33:41 +0530

    fix: fix folder names

--- flowspace-frontend/src/App.tsx
+++ flowspace-frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import { BrowserRouter,Route, Routes } from "react-router-dom";
-import Dashboard from "./components/dasboard/dashboard";
+import Dashboard from "./components/dashboard/dashboard";
 
 function App() {
   return (
